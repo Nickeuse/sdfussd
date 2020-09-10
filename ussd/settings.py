1ebc7c6431451a1852e390ea55f286c6b2bb853d
unknown <tuganimana01@gmail.com>
2020-09-10 11:02:21 +0200

    test

--- ussd/settings.py
+++ ussd/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    # '/var/www/static/',
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
 ]
